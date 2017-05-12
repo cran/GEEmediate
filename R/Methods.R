c21ea809fc1e04ec17ac6710657c95630037fe48
Daniel Nevo <danielnevo@gmail.com>
2017-05-12 23:18:56 +0000

    version 1.1.1

--- R/Methods.R
+++ R/Methods.R
@@ -20,11 +20,12 @@ print.GEEmediate <- function(x, digits = max(options()$digits - 4, 3),...)
     pvalue <- 2*pnorm(abs(zvalue),lower.tail = F)
     coef.table <- cbind(coeffs, sd.err, zvalue, pvalue)
     dimnames(coef.table) <- list(coeffs.names,
-                                 c("EStimate", "Std. Error", "z value", "Pr(>|z|)"))
+                                 c("Estimate", "Std. Error", "z value", "Pr(>|z|)"))
     printCoefmat(coef.table, digits = digits)
   } else if (x$pres=="sep") {
-    cat("Conditional Model (Model with the Mediator):\n \n")
-    cat("Coefficients:\n")
+    cat("------------------------------------------------------------------------------")
+    cat("\n------------------------------------------------------------------------------")
+    cat("\nMarginal Model (Model without the Mediator):\n")
     coeffs <- x$GEEfit$coefficients
     coeffs.names <- names(coeffs)
     stars <- which(sapply(coeffs.names, substrRight)==".star")
@@ -35,10 +36,18 @@ print.GEEmediate <- function(x, digits = max(options()$digits - 4, 3),...)
     pvalue <- 2*pnorm(abs(zvalue),lower.tail = F)
     coef.table <- cbind(coeffs, sd.err, zvalue, pvalue)
     dimnames(coef.table) <- list(names(coeffs),
-                                 c("EStimate", "Std. Error", "z value", "Pr(>|z|)"))
-    stats::printCoefmat(coef.table[stars,], digits = digits)
-    cat("\nMarginal Model (Model without the Mediator):\n")
-    stats::printCoefmat(coef.table[no.stars,],  digits = digits)
+                                 c("Estimate", "Std. Error", "z value", "Pr(>|z|)"))
+    cond.table <- coef.table[no.stars,]
+    rownames(cond.table)[1] <- "(Intercept)"
+    marg.table <- coef.table[stars,]
+    rownames(marg.table)[1] <- "(Intercept)"
+    rownames(marg.table)[-1] <- sapply(rownames(marg.table)[-1], function(x) substr(x,1,nchar(x)-5))
+    #cat(row.names(marg.table), "\n")
+    stats::printCoefmat(marg.table, digits = digits)
+    cat("\n------------------------------------------------------------------------------")
+    cat("\n------------------------------------------------------------------------------")
+    cat("\nConditional Model (Model with the Mediator):\n \n")
+    stats::printCoefmat(cond.table,  digits = digits)
   }
   cat("\n---------------------------")
   cat("\nNatural Indirect Effect: ", format(x$nie, digits = digits),
@@ -46,11 +55,49 @@ print.GEEmediate <- function(x, digits = max(options()$digits - 4, 3),...)
       " for ", paste0(x$alter), " test for mediation \n", sep = "")
   cat("Confidence Interval = [", format(x$nie.ci[1],digits = digits),",",format(x$nie.ci[2],digits = digits),"]", sep = "")
   cat("\n---------------------------")
-  cat("\nNatural Direct Effect:", format(x$nie, digits = digits))
+  cat("\nNatural Direct Effect:", format(x$nde, digits = digits))
   cat("\n---------------------------")
+  if(x$pm >=0 & x$pm < 1)
+  {
   cat("\nMediation Proportion:", format(100*x$pm,digits = 3),"%",
       "\np=", format.pval(x$pm.pval, digits = 2),
       " for one-sided test for mediation \n", sep = "")
+
   cat("Confidence Interval = [", format(100*x$pm.ci[1],digits = 3),"%",",",format(100*x$pm.ci[2],digits = 3),"%","]", sep = "")
-  cat("\n---------------------------")
+  } else
+  {
+    cat("\nMediation Proportion:", format(100*x$pm,digits = 3),"%")
+  }
+   cat("\n---------------------------")
+}
+#' @export
+predict.GEEmediate <- function (object, newdata = NULL, model.pred = c("cond", "marg"),type = c("link", "response", "terms"),
+                                se.fit = FALSE, dispersion = NULL, terms = NULL, na.action = na.pass, ...)
+{
+  model.pred <- match.arg(model.pred)
+  type <- match.arg(type)
+  if (se.fit==T) {warning("se.fit=T is currently not supported for GEEmediate")}
+  if (type=="terms") {
+    warning("type='terms' is currently not supported for GEEmediate, using type='response' intead")
+    type <- "response"
+  }
+  gee.object <- object$GEEfit
+  if(!missing(newdata))
+  {
+    newdf <- newdata[gee.object$xnames[substrRight(gee.object$xnames)!=".star" & substrRight(gee.object$xnames)!="INT"]]
+    dupl.df.new <- DupliData(df = newdf, mediator = object$call[[4]], surv = F)
+    dupl.df.new <- dupl.df.new[gee.object$xnames]
+    eta <- gee.object$linear.predictors
+    if (type=="response") {
+    pred <- make.link(tolower(gee.object$model$link))$linkinv(eta)
+    } else {
+      pred <- eta
+    }
+    if (model.pred=="cond") {out <- pred[1:(gee.object$nobs/2)]}
+    if (model.pred=="marg") {out <- pred[(gee.object$nobs/2+1):gee.object$nobs]}
+  } else {
+    pred <- predict.glm(object = gee.object, type = type, se.fit = F, na.action = na.action, ...)
+    if (model.pred=="cond") {out <- pred[1:(gee.object$nobs/2)]}
+    if (model.pred=="marg") {out <- pred[(gee.object$nobs/2+1):gee.object$nobs]}}
+  out
 }
